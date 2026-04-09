cce54f2fb148a2151d8baae2f3aea7cb9e863fb5
Bryan Valverde Hernández <bvalverde20671@ufide.ac.cr>
2026-04-09 00:57:48 -0600

    Agrega modelo Categoria

---
+++ ProyectoTuristico-main/turistico/Models/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace turistico.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [StringLength(300)]
+        public string Descripcion { get; set; }
+
+        public virtual ICollection<Lugar> Lugares { get; set; }
+    }
+}
