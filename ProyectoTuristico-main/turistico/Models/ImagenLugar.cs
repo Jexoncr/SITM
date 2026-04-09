d5ccc1e1b571bc5fe7901984f6929936ee3a0188
Bryan Valverde Hernández <bvalverde20671@ufide.ac.cr>
2026-04-09 00:57:48 -0600

    Agrega modelo ImagenLugar

---
+++ ProyectoTuristico-main/turistico/Models/ImagenLugar.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace turistico.Models
+{
+    public class ImagenLugar
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int LugarId { get; set; }
+
+        [ForeignKey(nameof(LugarId))]
+        public virtual Lugar Lugar { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string UrlImagen { get; set; }
+    }
+}
