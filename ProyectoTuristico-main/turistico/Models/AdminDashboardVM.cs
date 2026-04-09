11432a82e85be123ac404b75e2024b9002f98b7b
Bryan Valverde Hernández <bvalverde20671@ufide.ac.cr>
2026-04-09 00:57:48 -0600

    Agrega AdminDashboardVM

---
+++ ProyectoTuristico-main/turistico/Models/AdminDashboardVM.cs
@@ -0,0 +1,15 @@
+﻿namespace turistico.Models
+{
+    public class AdminDashboardVM
+    {
+        public int ComerciosRegistrados { get; set; }
+        public int ComerciosPendientes { get; set; }
+        public int ComerciosAprobados { get; set; }
+        public int ComerciosRechazados { get; set; }
+
+        public int EventosActivos { get; set; }
+        public int UsuariosRegistrados { get; set; }
+        public int ResenasPendientes { get; set; }
+        public int ReservasRegistradas { get; set; }
+    }
+}
