837d45350c3fdbdec5ca22f34cc9a7ad748ec2cc
Sofiass10 <Sofiasotsan10@icloud.com>
2026-04-09 12:35:10 -0600

    Agrega modelo AccionAuditoria

---
+++ ProyectoTuristico-main/turistico/Models/AccionAuditoria.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace turistico.Models
+{
+    public class AccionAuditoria
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string AdminUserId { get; set; }
+
+        [ForeignKey(nameof(AdminUserId))]
+        public virtual ApplicationUser AdminUser { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string EntidadAfectada { get; set; }
+
+        public int IdEntidad { get; set; }
+
+        [StringLength(100)]
+        public string TipoAccion { get; set; }
+
+        [Required]
+        public DateTime Fecha { get; set; }
+    }
+}
