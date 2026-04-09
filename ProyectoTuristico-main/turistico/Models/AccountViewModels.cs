3eb39a61a741860acf3728c886120b054d0a5a92
Sofiass10 <Sofiasotsan10@icloud.com>
2026-04-09 12:35:10 -0600

    Agrega AccountViewModels

---
+++ ProyectoTuristico-main/turistico/Models/AccountViewModels.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace turistico.Models
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage = "El correo es obligatorio.")]
+        [EmailAddress(ErrorMessage = "Correo inválido.")]
+        [Display(Name = "Correo")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [Display(Name = "Recordarme")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterVM
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [StringLength(100)]
+        [Display(Name = "Nombre")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido es obligatorio.")]
+        [StringLength(100)]
+        [Display(Name = "Apellido")]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "El correo es obligatorio.")]
+        [EmailAddress(ErrorMessage = "Correo inválido.")]
+        [Display(Name = "Correo")]
+        public string Email { get; set; }
+
+        [Phone]
+        [Display(Name = "Teléfono")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria.")]
+        [StringLength(100, ErrorMessage = "La contraseña debe tener al menos {2} caracteres.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Debes confirmar la contraseña.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar contraseña")]
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "Las contraseñas no coinciden.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
